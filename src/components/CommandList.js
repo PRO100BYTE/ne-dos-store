502963d58b2ea1eefa040e9cd2faf5f4d4f63242
Evgeniy Struchkov <thedayg0ne@internet.ru>
2023-08-14 15:06:25 +0700

    Update CommandList

--- src/components/CommandList.js
+++ src/components/CommandList.js
@@ -5,55 +5,66 @@ function CommandList() {
   // Массив с данными о дополнениях (командах)
   const commands = [
     {
-      name: 'calc',
-      description: 'Калькулятор, позволяющий выполнять арифметические операции',
-      install: 'registercommand https://nedos-store.com/calc'
+        name: 'calc',
+        description: 'Калькулятор, позволяющий выполнять арифметические операции',
+        install: 'registercommand https://store.ne-dos.ru/calc'
     },
     {
-      name: 'clock',
-      description: 'Часы, показывающие текущее время и дату',
-      install: 'registercommand https://nedos-store.com/clock'
+        name: 'clock',
+        description: 'Часы, показывающие текущее время и дату',
+        install: 'registercommand https://store.ne-dos.ru/clock'
     },
     {
-      name: 'snake',
-      description: 'Классическая игра Змейка, в которой нужно собирать еду и не врезаться в стены или хвост',
-      install: 'registercommand https://nedos-store.com/snake'
+        name: 'snake',
+        description: 'Классическая игра Змейка, в которой нужно собирать еду и не врезаться в стены или хвост',
+        install: 'registercommand https://store.ne-dos.ru/snake'
     },
     {
-      name: 'chat',
-      description: 'Чат, позволяющий общаться с другими пользователями NE-DOS',
-      install: 'registercommand https://nedos-store.com/chat'
+        name: 'chat',
+        description: 'Чат, позволяющий общаться с другими пользователями NE-DOS',
+        install: 'registercommand https://store.ne-dos.ru/chat'
     },
     {
-      name: 'paint',
-      description: 'Графический редактор, позволяющий рисовать и сохранять изображения',
-      install: 'registercommand https://nedos-store.com/paint'
+        name: 'paint',
+        description: 'Графический редактор, позволяющий рисовать и сохранять изображения',
+        install: 'registercommand https://store.ne-dos.ru/paint'
     },
     {
-      name: 'music',
-      description: 'Музыкальный плеер, позволяющий воспроизводить и загружать музыкальные файлы',
-      install: 'registercommand https://nedos-store.com/music'
+        name: 'music',
+        description: 'Музыкальный плеер, позволяющий воспроизводить и загружать музыкальные файлы',
+        install: 'registercommand https://store.ne-dos.ru/music'
     },
     {
-      name: 'browser',
-      description: 'Веб-браузер, позволяющий просматривать веб-страницы в интернете',
-      install: 'registercommand https://nedos-store.com/browser'
+        name: 'browser',
+        description: 'Веб-браузер, позволяющий просматривать веб-страницы в интернете',
+        install: 'registercommand https://store.ne-dos.ru/browser'
     },
     {
-      name: 'todo',
-      description: 'Список дел, позволяющий добавлять, удалять и отмечать задачи',
-      install: 'registercommand https://nedos-store.com/todo'
+        name: 'todo',
+        description: 'Список дел, позволяющий добавлять, удалять и отмечать задачи',
+        install: 'registercommand https://store.ne-dos.ru/todo'
     },
     {
-      name: 'weather',
-      description: 'Погода, показывающая текущую температуру, влажность и прогноз на ближайшие дни',
-      install: 'registercommand https://nedos-store.com/weather'
+        name: 'weather',
+        description: 'Погода, показывающая текущую температуру, влажность и прогноз на ближайшие дни',
+        install: 'registercommand https://store.ne-dos.ru/weather'
     },
     {
-      name: 'quiz',
-      description: 'Викторина, в которой нужно отвечать на вопросы по разным темам',
-      install: 'registercommand https://nedos-store.com/quiz'
+        name: 'quiz',
+        description: 'Викторина, в которой нужно отвечать на вопросы по разным темам',
+        install: 'registercommand https://store.ne-dos.ru/quiz'
+    },
+    {
+        name: 'www',
+        description: 'Открывает ссылку в новой вкладке',
+        install: 'registercommand https://store.ne-dos.ru/www'
+    },
+    {
+        name: 'github',
+        description: 'Открывает репозиторий проекта на GitHub',
+        install: 'registercommand https://store.ne-dos.ru/github'
     }
+    
   ];
 
   return (
